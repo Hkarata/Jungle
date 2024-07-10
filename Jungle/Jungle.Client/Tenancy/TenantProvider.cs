ecafb63d2c420cbf76f42f51f86b6f17fe82182b
hei karata <herishine00@gmail.com>
2024-07-10 16:54:14 +0300

    Add project files.

---
+++ Jungle/Jungle.Client/Tenancy/TenantProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Jungle.Client.Tenancy
+{
+    public sealed class TenantProvider
+    {
+        private const string TenantIdHeaderName = "X-TenantId";
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TenantProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? TenantId => _httpContextAccessor
+            .HttpContext
+            .Request
+            .Headers[TenantIdHeaderName];
+    }
+}

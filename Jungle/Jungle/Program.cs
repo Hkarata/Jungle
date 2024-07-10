11c0fca12d654e2795cd08ea010ce02c5eeed2ed
hei karata <herishine00@gmail.com>
2024-07-10 18:11:01 +0300

    Enhanced authentication and user management
    This commit significantly upgrades the application's authentication system and user account management capabilities. Key changes include:
    
    - Updated NuGet package references in `Jungle.Client.csproj` and `Jungle.csproj` to version `8.0.7`, adding new packages for Entity Framework Core and ASP.NET Core Identity.
    - Added `UserSecretsId` in `Jungle.csproj` for secure storage of sensitive information.
    - Modified `Program.cs` to incorporate services for authentication and Entity Framework Core, including setting up `ApplicationDbContext` and configuring identity services.
    - Introduced new endpoints in `IdentityComponentsEndpointRouteBuilderExtensions.cs` for identity-related actions and added a `PersistentAuthenticationStateProvider` for client-side authentication state management.
    - Implemented various classes and Razor components to support identity operations, such as `IdentityNoOpEmailSender`, `IdentityRedirectManager`, `IdentityUserAccessor`, and pages for account actions (e.g., login, register, password reset).
    - Added comprehensive user account management functionality with Razor pages for password reset, email confirmation, account deletion, and two-factor authentication setup.
    - Introduced new Razor pages and components for managing user profiles, personal data, and authentication settings, enhancing security and user experience.
    - Created a new migration `InitialMigration` for initial database setup, integrating ASP.NET Core Identity framework and configuring model relationships and indexes for optimization.
    
    These enhancements not only update package dependencies but also introduce a robust framework for managing user accounts, authentication, and security within the application.

--- Jungle/Jungle/Program.cs
+++ Jungle/Jungle/Program.cs
@@ -1,4 +1,9 @@
 using Jungle.Components;
+using Jungle.Components.Account;
+using Jungle.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +11,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
 var app = builder.Build();
 
@@ -30,5 +63,8 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Jungle.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
 
 app.Run();

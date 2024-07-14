3d2e9216e967d97aad01ac48ba75f557a80dda55
hei karata <herishine00@gmail.com>
2024-07-14 17:01:37 +0300

    Add DynamoDB support and OTLP exporter setup
    - Added AWSSDK.DynamoDBv2 package to Jungle.AppHost.csproj for AWS DynamoDB support.
    - Configured a local DynamoDB container in Program.cs using `amazon/dynamodb-local` image, with HTTP/HTTPS endpoints and ports mapping.
    - Added OTLP exporter setup for exporting telemetry data.
    - Included a new project reference to `Projects.Jungle_Api` with alias "Jungle-api".

--- Jungle.AppHost/Program.cs
+++ Jungle.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var dynamoDb = builder.AddContainer("DynamoDb", "amazon/dynamodb-local")
+    .WithHttpEndpoint(8000, 8002)
+    .WithHttpsEndpoint(8001, 8003)
+    .WithOtlpExporter();
+
 var apiService = builder.AddProject<Projects.Jungle_Api>("Jungle-api");
 
 builder.AddProject<Projects.Jungle>("Jungle-web")

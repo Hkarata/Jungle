a61fbf500812fa0afe177b9156b955b4fa75b71e
hei karata <herishine00@gmail.com>
2024-07-16 14:08:00 +0300

    Enhance UI and add image slideshow
    - MainLayout.razor.css: Improved header layout with `justify-content`, increased gap in `#Links`, made `main` content area flexible, and hid scrollbar. Adjusted padding in `main` for mobile view.
    - Home.razor: Added `<PageTitle>` component, adjusted font size in `#left`, replaced placeholder with image in `#right`, and added script for image slideshow.
    - Home.razor.css: Removed `border-radius` from `#First`, adjusted `#right` and `#placeOrder` styling for clarity and responsiveness. Enhanced responsive design for `#slideshow`, `#First`, `#left`, and `#right`.
    - Jungle.csproj: Included `wwwroot\js\` folder reference for JavaScript files.
    - Added simple image slideshow functionality in `/js/image-slideshow.js`, cycling through images in `#slideshow`.

---
+++ Jungle/Jungle/wwwroot/js/image-slideshow.js
@@ -0,0 +1,12 @@
+﻿var images = ['/Images/shirts.png', '/Images/beats.png', '/Images/shoes.png']; // Add your image paths here
+var index = 0;
+
+function changeImage() {
+    var imgElement = document.getElementById('slideshow');
+    imgElement.src = images[index];
+    index = (index + 1) % images.length;
+}
+
+window.onload = function () {
+    setInterval(changeImage, 3000); // Change image every 3 seconds
+};
